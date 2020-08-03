a5420d607e1d84b4f462e16edf282a53ae3faa99
leety <ltyiz07@gmail.com>
2020-08-03 15:09:41 +0900

    08/03 13:09

--- gugudan_3.py
+++ gugudan_3.py
@@ -1,11 +1,8 @@
-#모든 '3'을 '*'으로 출력
-#gugudan 과의 다른점은 gugudan은 단순히 "3"만을 if == 3 으로 확인해 줄력하는 반면
-#gugudan_3은 모든 출력에서 '3'을 모두 검사해 '3'일때 '*' 출력.
-<<<<<<< HEAD
-f = open("d:\\test\\gugudan_3to_star.txt", 'w')
-=======
+# 모든 '3'을 '*'으로 출력
+# gugudan 과의 다른점은 gugudan은 단순히 "3"만을 if == 3 으로 확인해 줄력하는 반면
+# gugudan_3은 모든 출력에서 '3'을 모두 검사해 '3'일때 '*' 출력.
 f = open("c:\\pyth\\gugudan_3to_star.txt", 'w')
->>>>>>> b2955f0282ff77dbcb8789c7efdb099f956a6a2e
+
 data = ''
 for i in range(1, 10, 1):
     for j in [2, 3, 4]:
@@ -13,7 +10,7 @@ for i in range(1, 10, 1):
         data = "{0:>2} x {1:>2} = {2:>2}     ".format(j, i, j * i)
         for k in data:
             if k == '3':
-                data = data[:count] + '*' + data[count + 1 :]
+                data = data[:count] + '*' + data[count + 1:]
             count = count + 1
         f.write(data)
     f.write("\n")
@@ -46,4 +43,3 @@ for i in range(1, 10, 1):
         f.write(data)
     f.write("\n")
 f.write("\n")
-
