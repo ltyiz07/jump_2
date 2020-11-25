2031efd47a19194940fb7c676718088e75c6cb7d
Young <ltyiz07@gmail.com>
2020-11-25 18:20:45 +0900

    time problem with algo.even/

--- algorithm_practice/even/main.py
+++ algorithm_practice/even/main.py
@@ -1,6 +1,7 @@
 from itertools import product
 from itertools import combinations
 import numpy as np
+from math import comb
 
 
 def solution(a):
@@ -12,27 +13,42 @@ def solution(a):
         for j, m in enumerate(l):
             if m == 1:
                 one_count[j] += 1
+    print("one_count: ", one_count)
 
     all_ = []
+    comb_lst = []
     for o in one_count:
-        all_.append(combinations(range(row), o))
-        print('*' * 50)
-        for l in combinations(range(row), o):
-            print(l)
-    all_all = product(*all_)
+        comb_lst.append(comb(row, o))
+    large = max(comb_lst)
+    large_index = comb_lst.index(max(comb_lst))
+    print(large, "   ", large_index)
+
+    for i, o in enumerate(one_count):
+        if i == large_index:
+            all_.append((tuple(i for i in range(one_count[large_index])),))
+            continue
+        all_.append(tuple(combinations(range(row), o)))
+    print(all_)
+
+    all_all = tuple(product(*all_))
+    print(all_all)
     for l in all_all:
         temp_set = set()
         for m in l:
+            if not m:
+                continue
             temp_set = set(m).symmetric_difference(temp_set)
         if not temp_set:
             count += 1
+    count *= large
+    count = count % (10 ** 7 + 19)
 
     return count
 
 
 if __name__ == "__main__":
-    print(solution([[0,1,0],[1,1,1],[1,1,0],[0,1,1]]))
-    print('*' * 50)
+    # print(solution([[0,1,0],[1,1,1],[1,1,0],[0,1,1]]))
+    # print('*' * 50)
     # solution([[1,0,0],[1,0,0]])
     print('*' * 50)
-    # solution([[1,0,0,1,1],[0,0,0,0,0],[1,1,0,0,0],[0,0,0,0,1]])
+    print(solution([[1,0,0,1,1],[0,0,0,0,0],[1,1,0,0,0],[0,0,0,0,1]]))
